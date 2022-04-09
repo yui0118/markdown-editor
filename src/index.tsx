4e8482c7e4dc81dc05afa91d96067e75d85dfc4d
yui0118 <yui.orange.hello@gmail.com>
2022-04-09 16:37:48 +0900

    Fix: バグ修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,1 @@
-// JSXを使う場合、Reactライブラリのインポート必須(モジュールの全てをインポートする場合は * as fooを使う)
-// React-domライブラリのインポート
-import * as React from "react";
-import { render } from "react-dom";
-
-const Main = <h1>Markdown Editor</h1>; // Main という変数に React のビューをJSX記法で記述
-
-render(Main, document.getElementById("app")); // appというIDを持つHTML内の要素に対してMainという変数の内容をレンダリングで紐付ける処理
+console.log("yui");
