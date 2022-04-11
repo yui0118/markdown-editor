bf700aecd774adba55cb229e121f4a5f327249d9
yui0118 <yui.orange.hello@gmail.com>
2022-04-11 15:28:58 +0900

    Fix: Reactのビューが読み込まれないエラーを修正2

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.tsx", // 1番最初に読み込むファイル(エントリポイント)のパス
+  entry: './src/index.tsx', // 1番最初に読み込むファイル(エントリポイント)のパス
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: ['.js', '.ts', '.tsx'],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
-    publicPath: "dist/",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: 'dist/',
   },
 };
