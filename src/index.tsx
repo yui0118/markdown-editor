64f942887ccc21e2251d72e77acf8cae2d37493f
yui0118 <yui.orange.hello@gmail.com>
2022-04-12 23:58:02 +0900

    Add: 関数コンポーネントEditorでエディタ画面の見た目を作る

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import { render } from "react-dom";
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components"; // styled-components の createGlobalStyleをインポート
+import { Editor } from "./pages/editor"; // 作成したエディタ画面(コンポーネント)をインポート
 
-const Header = styled.h1`
-  color: red;
+// styled-components の createGlobalStyle を使って、ページ全体に適用できるスタイルを定義
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
 `;
-
-const Main = <Header>Markdown Editor</Header>;
+const Main = (
+  <>
+    <GlobalStyle /> {/* GlobalStyleコンポーネントを読み込む */}
+    <Editor /> {/* インポートしたEditorコンポーネントを読み込む */}
+  </>
+);
 
 render(Main, document.getElementById("app"));
